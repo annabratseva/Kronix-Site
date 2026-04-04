5728868b5d7cbf988a6c9f633deb231fbfb696fa
annabratseva <anna.bratseva.student@gmail.com>
2026-04-05 04:09:47 +0500

    Сделала интерфейс бука

--- js/modules/book-call.js
+++ js/modules/book-call.js
@@ -2,6 +2,7 @@ export const bookCallFunc = () => {
     const bookACall = document.querySelector(".green-button--smaller")
     const callMenu = document.querySelector(".book-call")
     const closeMenu = document.querySelector(".book-call__close")
+    
 
     bookACall.addEventListener("click", () => {
         callMenu.classList.add("showBook")
