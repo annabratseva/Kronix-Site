d62ade8fb34a54660c996e830fb96e685449ca20
annabratseva <anna.bratseva.student@gmail.com>
2026-04-05 03:53:10 +0500

    Сделала интерфейс бука

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ import {openMenu} from "./modules/header.js"
 import { scrollTop } from "./modules/scrollTop.js";
 import { buttonClick } from "./modules/scrollTop.js";
 import { faqCheck } from "./modules/checkInput.js";
+import { bookCallFunc } from "./modules/book-call.js";
 
+bookCallFunc()
 faqCheck()
 
 window.openMenu = openMenu;
@@ -10,7 +12,7 @@ window.buttonClick = buttonClick;
 
 const elements = document.querySelectorAll('.fade-in');
 
-// для появления элементов и т.д
+// для появления эл. и т.д
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
---
+++ js/modules/book-call.js
@@ -0,0 +1,17 @@
+export const bookCallFunc = () => {
+    const bookACall = document.querySelector(".green-button--smaller")
+    const callMenu = document.querySelector(".book-call")
+    const closeMenu = document.querySelector(".book-call__close")
+
+    bookACall.addEventListener("click", () => {
+        callMenu.classList.add("showBook")
+    })
+
+    closeMenu.addEventListener("click", () => {
+        callMenu.classList.remove("showBook")
+    })
+
+    
+}
+
+
