8fb69762df4b177ba1996d1fcfc20f442ec95e35
annabratseva <anna.bratseva.student@gmail.com>
2026-04-05 16:07:23 +0500

    Сделала интерфейс бука

--- js/index.js
+++ js/index.js
@@ -3,25 +3,14 @@ import { scrollTop } from "./modules/scrollTop.js";
 import { buttonClick } from "./modules/scrollTop.js";
 import { faqCheck } from "./modules/checkInput.js";
 import { bookCallFunc } from "./modules/book-call.js";
+import { mouseMoveTwo } from "./modules/mousemove.js";
 
+mouseMoveTwo()
 bookCallFunc()
 faqCheck()
 
 window.openMenu = openMenu;
 window.buttonClick = buttonClick;
-
-const title = document.querySelector(".hero-block__text-cont")
-
-window.addEventListener("mousemove", (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5)
-    const y = (e.clientY / window.innerHeight - 0.5)
-
-    const moveX = x * 30
-    const moveY = y * 20
-
-    title.style.transform = `translate(${moveX}px, ${moveY}px)`
-})
-
 
 const elements = document.querySelectorAll('.fade-in');
 
@@ -36,4 +25,6 @@ const observer = new IntersectionObserver((entries) => {
 
 
 elements.forEach(el => observer.observe(el));
+
+
 
---
+++ js/modules/mousemove.js
@@ -0,0 +1,25 @@
+export const mouseMoveTwo = () => {
+  const title = document.querySelector(".hero-block__text-cont")
+  const banner = document.querySelector(".banner__inner-cont")
+  const greenButtonNew = document.querySelector(".green-button--new")
+
+  const items = [
+    { el: title, speedX: 30, speedY: 20 },
+    { el: banner, speedX: 50, speedY: 50 },
+    { el: greenButtonNew, speedX: 40, speedY: 40}
+  ]
+
+  window.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5)
+    const y = (e.clientY / window.innerHeight - 0.5)
+
+    items.forEach(item => {
+      if (!item.el) return
+
+      const moveX = x * item.speedX
+      const moveY = y * item.speedY
+
+      item.el.style.transform = `translate(${moveX}px, ${moveY}px)`
+    })
+  })
+}
